ac9dd114eac76b5e5aef40a7b1001632de4466a6
alex.anoshkin <alex.anoshkin@cyoda.com>
2022-07-28 21:14:31 +0300

    feature/test-github-actions: test github actions

---
+++ src/test/java/com/axel_n/limiter/ExampleBrokenTest.java
@@ -0,0 +1,14 @@
+package com.axel_n.limiter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleBrokenTest {
+
+    // TODO delete branch after tests
+    @Test
+    void brokenTest() {
+        assertTrue(false);
+    }
+}
