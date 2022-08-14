ffc96929261dde39f8832b4fcf1f9d4894a3481f
alex.anoshkin <alex.anoshkin@cyoda.com>
2022-08-14 20:21:19 +0300

    fix right calculate max floor execution time

--- src/test/java/io/github/axel_n/limiter/test/utils/NumberUtils.java
+++ src/test/java/io/github/axel_n/limiter/test/utils/NumberUtils.java
@@ -6,14 +6,14 @@ public class NumberUtils {
     }
 
     public static double calculateMaxFloorExecutionTime(double executionTimeSeconds) {
-        double percentOfMaxFloor = 0.1;
+        double percentOfMaxFloor = 0.3;
 
         double somePercent = (executionTimeSeconds / 100) * percentOfMaxFloor;
         return somePercent + executionTimeSeconds;
     }
 
-    public static double getApproximatedExecutionTime(int allRequests, int maxRequestsPerPeriod) {
-        return (double) allRequests / (double) maxRequestsPerPeriod - 1;
+    public static double getApproximatedExecutionTime(int allRequests, int maxRequestsPerPeriod, int intervalSeconds) {
+        return ((double) allRequests / (double) maxRequestsPerPeriod - 1) * (double) intervalSeconds;
     }
 
 }
